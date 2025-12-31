7a67ff58823f502636e788ea08432b0ed5316f53
kkv123 <104729596+kkv123@users.noreply.github.com>
2025-12-31 20:32:07 +0530

    theme switcher using contextHandler react

---
+++ src/context/theme.js
@@ -0,0 +1,14 @@
+import { useContext, createContext } from "react";
+
+
+export const ThemeContext = createContext({
+    themeMode: "dark",
+    lightTheme: () => { },
+    darkTheme: () => { }
+})
+
+export const ThemeProvider = ThemeContext.Provider;
+
+export default function useTheme() {
+    return useContext(ThemeContext);
+}
